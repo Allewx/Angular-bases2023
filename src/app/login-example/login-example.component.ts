6fd921a650d6be0367addc873747f1c77b9b0e28
Alex <alex.guillin@nexaware.com.ec>
2023-07-17 23:16:22 -0500

    Primer paso

--- src/app/login-example/login-example.component.ts
+++ src/app/login-example/login-example.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-example',
   templateUrl: './login-example.component.html',
-  styleUrls: ['./login-example.component.css','./login-example.component.util.css']
+  styleUrls: ['./login-example.component.css']
 
 })
 export class LoginExampleComponent {
